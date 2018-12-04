8b21d137f9579aee201360e397c789984dc6404f
jckling <jckling@163.com>
2018-12-05 00:04:44 +0800

    提交ns-0x09实验报告

---
+++ ns-0x09/guardian/guardian_unblock.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# this is a sample unblock script for guardian. This should work with ipchains. 
+# This command gets called by guardian as such:
+#  unblock.sh <source_ip> <interface>
+# and the script will issue a command to remove the block that was created with # block.sh address. 
+
+source=$1
+interface=$2
+
+/sbin/iptables -D INPUT -s $source -i $interface -j DROP
+/sbin/iptables -D INPUT -p tcp -s $source -i $interface -j REJECT --reject-with tcp-reset
